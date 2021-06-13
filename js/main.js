1b52376ebf2b265e453060350dcb0b6f7d52ad7a
Marathe-Suraj <85793485+Marathe-Suraj@users.noreply.github.com>
2021-06-13 11:02:08 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,77 @@
+/*======================= Mobile View=================== */
+let nav = document.querySelector("nav");
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let logoOne = document.querySelector(".logo");
+
+menuBtn.onclick = function () {
+  navBar.classList.add("active");
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  body.style.overflow = "hidden";
+};
+
+cancelBtn.onclick = function () {
+  navBar.classList.remove("active");
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  body.style.overflow = "auto";
+};
+
+/*======================= side navigation bar close when we click on navigation links ===================== */
+
+let navLinks = document.querySelectorAll(".menu li a");
+for (var i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+  });
+}
+
+/* ======================= Scroll fixed =================== */
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("sticky");
+    logoOne.classList.add("active");
+    menuBtn.classList.add("active");
+  } else {
+    nav.classList.remove("sticky");
+    logoOne.classList.remove("active");
+    menuBtn.classList.remove("active");
+  }
+
+  /*======================= Counter on Scroll =================== */
+
+  var number = 0;
+  var oTop = $("#counter").offset().top - window.innerHeight;
+
+  if (number == 0 && $(window).scrollTop() > oTop) {
+    $(".counter-value").each(function () {
+      var $this = $(this),
+        countTo = $this.attr("data-count");
+      $({
+        countNum: $this.text(),
+      }).animate(
+        {
+          countNum: countTo,
+        },
+
+        {
+          duration: 2000,
+          easing: "swing",
+          step: function () {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function () {
+            $this.text(this.countNum);
+          },
+        }
+      );
+    });
+  }
+};
